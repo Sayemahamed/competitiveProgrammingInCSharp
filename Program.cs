50a225d379994a7848b7c9442c5654d107b3ac63
Sayem <sayemahamed183@gmail.com>
2022-12-25 22:30:46 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class codeforces
+{
+    //Start of Input/Output Methods
+    static int[] getIntArr() => Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+    static long[]getLongArr()=>Console.ReadLine().Split(' ').Select(x=>Convert.ToInt64(x)).ToArray();
+    static double[]getDoubleArr()=>Console.ReadLine().Split(' ').Select(x=>Convert.ToDouble(x)).ToArray();
+    static string[]getWords()=>Console.ReadLine().Split(' ');
+    static void printArr<T>(T[] x) {for (var i = 0; i < x.Length; i++)Console.Write($"{x[i]} ");}
+    //End of Input/Output Methods
+
+
+
+    static void Main()
+    {
+        int[] intArr = getIntArr();
+    }
+}

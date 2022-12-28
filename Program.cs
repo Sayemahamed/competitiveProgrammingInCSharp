a8d6b511494e32b5def03c54068d2694cb1acb75
Sayem <sayemahamed183@gmail.com>
2022-12-28 13:38:18 +0600

    ok

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class codeforces
@@ -8,13 +9,37 @@ class codeforces
     static long[]getLongArr()=>Console.ReadLine().Split(' ').Select(x=>Convert.ToInt64(x)).ToArray();
     static double[]getDoubleArr()=>Console.ReadLine().Split(' ').Select(x=>Convert.ToDouble(x)).ToArray();
     static string[]getWords()=>Console.ReadLine().Split(' ');
-    static void printArr<T>(T[] x) {for (var i = 0; i < x.Length; i++)Console.Write($"{x[i]} ");}
+    static int getInt()=>Convert.ToInt32(Console.ReadLine().Trim());
+    static void printArr<T>(T[] x) {for (var i = 0; i < x.Length; i++)Console.Write($"{x[i]} ");Console.WriteLine();
+    }
     //End of Input/Output Methods
 
+
+    //Start of Auxiliary Function
+
+    //End of Auxiliary Function
+
+
+    //Start of Solve Function
+    static void solve()
+    {
+        string s= Console.ReadLine().Trim();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] != 'a' && s[i] != 'A' && s[i] != 'e' && s[i] != 'E' && s[i] != 'i' && s[i] != 'I' && s[i] != 'o' && s[i] != 'O' && s[i] != 'u' && s[i] != 'U' && s[i] != 'y' && s[i] != 'Y')
+                Console.Write($".{s[i].ToString().ToLower()}");
+        }
+    }
+    //End of Solve Function
 
 
     static void Main()
     {
-        int[] intArr = getIntArr();
+        int test = 1;
+        while(test-- > 0 )
+        {
+            solve();
+            
+        }
     }
 }
